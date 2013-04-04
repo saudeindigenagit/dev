5eae8115ad4a8dc6b23c0d4ee10eccbe4a303476
sidnei <sidnei@saudeindigena.com.br>
2013-04-04 11:19:22 -0300

    primeiro commit sidnei

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+ini_set('error_reporting', 2047);
+setlocale(LC_MONETARY,"pt_BR", "ptb");
+
+$GLOBALS['DB_PREFIXO'] = '';
+
+require_once "class/sys/Main.php";
+	
+ 
+$obj = new Main();
+
+
+?>
